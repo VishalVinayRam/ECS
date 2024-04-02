34ba3fecaded6cb6ddd14deb0c0973d1d0ff07ef
linuxlite <vishalvinayram5432@gmail.com>
2024-04-02 16:31:23 +0530

    before changes

--- bin/ecs.ts
+++ bin/ecs.ts
@@ -1,21 +1,67 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { EcsStack } from '../lib/ecs-stack';
+import ecs = require('aws-cdk-lib/aws-ecs');
+import ec2 = require('aws-cdk-lib/aws-ec2');
+import elbv2 = require('aws-cdk-lib/aws-elasticloadbalancingv2');
+import cdk = require('aws-cdk-lib');
 
 const app = new cdk.App();
-new EcsStack(app, 'EcsStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+const stack = new cdk.Stack(app, 'MyWebApp');
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+// Create a cluster
+const vpc = new ec2.Vpc(stack, 'Vpc', { maxAzs: 2, subnetConfiguration: [
+  {
+    cidrMask: 24,
+    name: 'public',
+    subnetType: ec2.SubnetType.PUBLIC,
+  }
+] });
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+const cluster = new ecs.Cluster(stack, 'NodeCluster7', { vpc });
+cluster.addCapacity('AutoScalingGroups', {
+  instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.MICRO),
+  desiredCapacity: 4
 });
+
+// Create Task Definition
+const taskDefinition = new ecs.Ec2TaskDefinition(stack, 'TaskDef');
+const container = taskDefinition.addContainer('web', {
+  image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
+  memoryLimitMiB: 256,
+});
+
+container.addPortMappings({
+  containerPort: 80,
+  hostPort: 8080,
+  protocol: ecs.Protocol.TCP
+});
+
+// Create Service
+const service = new ecs.Ec2Service(stack, "Service", {
+  cluster,
+  taskDefinition,
+});
+
+// Create ALB
+const lb = new elbv2.ApplicationLoadBalancer(stack, 'LBS', {
+  vpc,
+  internetFacing: true
+});
+const listener = lb.addListener('PublicListener', { port: 80, open: true });
+
+// Attach ALB to ECS Service
+listener.addTargets('ECS', {
+  port: 8080,
+  targets: [service.loadBalancerTarget({
+    containerName: 'web',
+    containerPort: 80
+  })],
+  // include health check (default is none)
+  healthCheck: {
+    interval: cdk.Duration.seconds(60),
+    path: "/health",
+    timeout: cdk.Duration.seconds(5),
+  }
+});
+
+new cdk.CfnOutput(stack, 'ALBNodeCluster7', { value: lb.loadBalancerDnsName, });
+
+app.synth();
